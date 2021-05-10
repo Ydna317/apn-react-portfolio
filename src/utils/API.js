9a90c076ea7a7f672531e3bd9201c3e5ade2ecf9
Andy Nguyen <Ydna317@gmail.com>
2021-05-10 16:16:11 -0700

    finalizing react portfolio.

--- src/utils/API.js
+++ src/utils/API.js
@@ -9,39 +9,60 @@ import jqueryLogo from "../assets/images/jqueryLogo.svg";
 import htmlLogo from "../assets/images/htmlLogo.svg";
 import cssLogo from "../assets/images/cssLogo.svg";
 import javascriptLogo from "../assets/images/javascriptLogo.svg";
+import budgetBoss from "../assets/images/budgetboss.png";
+import fitnessTracker from "../assets/images/fitnessTracker.png";
+import iShip from "../assets/images/iShip.png";
+import lifeCruiser from "../assets/images/lifeCruiser.png";
+
+
 
 const projects = [
     {
-        title: "Weather Dashboard",
-        description: "A weather dashboard app that shows the current and future weather information of a city.",
-        imageUrl: weatherDashboard,
+        title: "Budget Boss",
+        description: "Command your budget and stay organized with both online and offline features!",
+        imageUrl: budgetBoss,
+        githubUrl: "https://github.com/Ydna317/Budget-Boss",
+        deployedUrl: "https://budget-boss.herokuapp.com/"
     },
     {
-        title: "Employee Tracker",
-        description: "A command-line interface app for tracking employees within a company.",
+        title: "Employee Manager",
+        description: "Manage your staffing and roster with an integrated command line system. Our application will allow you to create, update, and delete anything from users, salaries, to departments and managers. The all in one employee manager.",
         imageUrl: employeeTracker,
+        githubUrl: "https://github.com/Ydna317/employeeManager",
+    },
+    {
+        title: "iShip",
+        description: "Compare your shipping needs and get the best rates from the most popular shipping companies out there.",
+        imageUrl: iShip,
+        githubUrl: "https://github.com/Noreldin-S/iship",
+        deployedUrl: "https://we-ship.herokuapp.com/"
+    },
+    {
+        title: "Fit Companion",
+        description: "Track your fitness goals and have a personalized record keeping of your physical success.",
+        imageUrl: fitnessTracker,
+        githubUrl: "https://github.com/Ydna317/fitCompanion",
+        deployedUrl: "https://fit-companion.herokuapp.com/"
+    },
+    {
+        title: "lifeCruiser",
+        description: "Your personal traveling record keeper. lifeCruiser allows you to create a history of your traveled places and allows you to find the best flights at any given notice to wherever you want to go!",
+        imageUrl: lifeCruiser,
+        githubUrl: "https://github.com/jrdrenth/LifeCruiser",
+        deployedUrl: "https://jrdrenth.github.io/LifeCruiser/index.html"
+    },
+    {
+        title: "One For All Weather",
+        description: "Know the best weather for any given place at any time! The app that helps you plan ahead!",
+        imageUrl: weatherDashboard,
+        githubUrl: "https://github.com/Ydna317/ofaWeather",
+        deployedUrl: "https://ydna317.github.io/ofaWeather/"
     },
     {
         title: "Google Book Search",
         description: "Online database for finding the perfect read.",
         imageUrl: googleBooks,
     },
-    {
-        title: "Employee Tracker",
-        description: "A command-line interface app for tracking employees within a company.",
-        imageUrl: employeeTracker,
-    },
-    {
-        title: "Employee Tracker",
-        description: "A command-line interface app for tracking employees within a company.",
-        imageUrl: employeeTracker,
-    },
-    {
-        title: "Employee Tracker",
-        description: "A command-line interface app for tracking employees within a company.",
-        imageUrl: employeeTracker,
-    }
-
         
 ];
 
