615bc4fba0a590fbc6f1fc55f52f75f7288f7c49
Andy Nguyen <Ydna317@gmail.com>
2021-05-10 18:22:29 -0700

    readjusting .gitignore

---
+++ build/precache-manifest.978552e6bea0b1da9247543372f2d25c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c214a60b71d734de30d3c0c1e858e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a96ed097cee2cba22f78",
+    "url": "/static/css/main.8318ada8.chunk.css"
+  },
+  {
+    "revision": "aa2dadd944ad6ac167b3",
+    "url": "/static/js/2.c70d0fb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c70d0fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96ed097cee2cba22f78",
+    "url": "/static/js/main.8edd345e.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "8182e19f1ba69b1f59815865262a4c6d",
+    "url": "/static/media/Andy P. Nguyen - Resume 03.10.21 ABK Data Entry Specialist.8182e19f.pdf"
+  },
+  {
+    "revision": "4b9f697527a4fa3933cd84e544fb3b7f",
+    "url": "/static/media/andypnguyen.4b9f6975.jpg"
+  },
+  {
+    "revision": "0eadfb9d791d105ebe4af615eb231950",
+    "url": "/static/media/budgetboss.0eadfb9d.png"
+  },
+  {
+    "revision": "555be697f2d9417d5d3d05038f063fd5",
+    "url": "/static/media/cssLogo.555be697.svg"
+  },
+  {
+    "revision": "fee19abc69dbf70aac5dea612aa1f791",
+    "url": "/static/media/employeeTracker.fee19abc.jpg"
+  },
+  {
+    "revision": "79ddc65e998e0ecbc74369513cc4bcaf",
+    "url": "/static/media/expressjsLogo.79ddc65e.svg"
+  },
+  {
+    "revision": "4c9e4a5b73caf2c6da21cbde956d5820",
+    "url": "/static/media/fitnessTracker.4c9e4a5b.png"
+  },
+  {
+    "revision": "4c400c66ccec22eac7181da96d4c6329",
+    "url": "/static/media/googleBooksSearch.4c400c66.jpg"
+  },
+  {
+    "revision": "0920c0efda6e5cd4b4356b74cdcbd5d5",
+    "url": "/static/media/htmlLogo.0920c0ef.svg"
+  },
+  {
+    "revision": "b399f7390843389de9df7fe4fd8f5e76",
+    "url": "/static/media/iShip.b399f739.png"
+  },
+  {
+    "revision": "1731f46f84c0ca27065bf14d90c1406e",
+    "url": "/static/media/javascriptLogo.1731f46f.svg"
+  },
+  {
+    "revision": "3b44b5b7cd86fd236835828e11fd9453",
+    "url": "/static/media/jqueryLogo.3b44b5b7.svg"
+  },
+  {
+    "revision": "ca03cf631b5de01ca2a228689e5a8d23",
+    "url": "/static/media/lifeCruiser.ca03cf63.png"
+  },
+  {
+    "revision": "93d52d4122199320ecf021913a894822",
+    "url": "/static/media/mongodbLogo.93d52d41.svg"
+  },
+  {
+    "revision": "d6f3f9c7938d1d8bee0f69de01fc2ebb",
+    "url": "/static/media/mysqlLogo.d6f3f9c7.svg"
+  },
+  {
+    "revision": "1602376fe7f93c99f06b20b57ce29044",
+    "url": "/static/media/nguyendesigns.1602376f.png"
+  },
+  {
+    "revision": "aebcfc8141e031943f5859fb64f54529",
+    "url": "/static/media/nodejsLogo.aebcfc81.svg"
+  },
+  {
+    "revision": "fce687f8625a5ca67b0545229f34eb7b",
+    "url": "/static/media/weatherDashboard.fce687f8.png"
+  }
+]);
